d638df19315a87edaa36b3b46cf2ab13e216c71b
Vadim Ulasevich <ulasevichvadimsergeevich@yandex.ru>
2022-03-20 11:40:46 +0300

    Lesson 18

--- app/src/main/java/com/example/lesson18homework/MainActivity.kt
+++ app/src/main/java/com/example/lesson18homework/MainActivity.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import java.lang.StringBuilder
 
 class MainActivity : AppCompatActivity() {
     private lateinit var displayTextView: TextView
@@ -32,23 +33,20 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
         displayTextView = findViewById(R.id.displayTextView)
     }
 
     fun onButtonClick(view: View) {
         val text = (view as Button).text.toString()
-
         Log.d(TAG, "User clicked on button: $text")
-
         when (text) {
+            "SIGN" -> handleChangeSignClick()
             "DEL" -> handleDelClick()
             "⌫" -> handleEraseClick()
             in DIGITS -> handleDigitClick(text)
             in SIGNS -> handleSignClick(text)
             "." -> handleDotClick()
             "=" -> handleEqualsClick()
-
         }
     }
 
@@ -57,8 +55,25 @@ class MainActivity : AppCompatActivity() {
         sign = null
         displayedText = "0"
     }
+    fun handleChangeSignClick() {
+        val minusSign = "-"
+        var positiveOrNegativeNumber: String = displayTextView.text.toString()
+        if(positiveOrNegativeNumber.contains(minusSign)){
+            var number : Int = positiveOrNegativeNumber.toInt()
+            var result : Int = number*(-1)
+            positiveOrNegativeNumber= result.toString()
+            displayTextView.text = positiveOrNegativeNumber
+        }else if (!(positiveOrNegativeNumber.contains(minusSign))){
+            positiveOrNegativeNumber = minusSign + positiveOrNegativeNumber
+            displayTextView.text = positiveOrNegativeNumber
+        }
+    }
 
     fun handleEraseClick() {
+        Log.d(
+            TAG, "clicked erase " +
+                    "displayedNumber=$displayedNumber"
+        )
         displayedText = displayedText.dropLast(1)
     }
 
@@ -78,7 +93,14 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun handleDotClick() {
-
+        val dot = "."
+        var dotNumber: String = displayTextView.text.toString()
+        if(dotNumber.contains(dot)){
+            displayTextView.text = dotNumber
+        }else{
+            dotNumber += dot
+            displayTextView.text = dotNumber
+        }
     }
 
     fun handleEqualsClick() {
@@ -88,28 +110,27 @@ class MainActivity : AppCompatActivity() {
                     "sign=${sign}, " +
                     "secondNumber=$displayedNumber"
         )
-
         val firstNumber = this.firstNumber ?: return
         val sign = this.sign ?: return
         val secondNumber = displayedNumber ?: return
-
-        val result = firstNumber + secondNumber
-
+        var result: Double? = null
+        when(sign){
+            "+" -> result = firstNumber + secondNumber
+            "-" -> result = firstNumber - secondNumber
+            "÷" -> result = firstNumber / secondNumber
+            "×" -> result = firstNumber * secondNumber
+        }
         this.firstNumber = null
         this.sign = null
         displayedNumber = result
     }
-
     companion object {
         private const val TAG = "MainActivity"
-
-
         private val DIGITS = listOf(
             "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"
         )
-
         private val SIGNS = listOf(
-            "+", "-", "*", "/"
+            "+", "-", "÷", "×"
         )
     }
 }
